48f4385361d8f5485d10d08eabf546ef4268d2eb
Catalina <150844717+Ctlna@users.noreply.github.com.>
2024-09-19 19:02:24 -0500

    feat: :sparkles: Se sube lo hecho

---
+++ Dia_04/ActividadTarea/sistemaRestaurantes/src/main/java/com/example/sistemaRestaurantes/service/RestaurantManager.java
@@ -0,0 +1,13 @@
+package com.example.sistemaRestaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRestaurantesApplication.class, args);
+	}
+
+}
